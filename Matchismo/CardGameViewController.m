3283aac43bb5ac73281f1eb949da03b25ef392f0
Neo Lee <neo.lee@gmail.com>
2014-08-03 13:03:26 +0800

    Stage 1: in class demo

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,11 +7,44 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation CardGameViewController
+
+-(Deck *)deck {
+    if (!_deck) _deck = [self createDeck];
+    return _deck;
+}
+
+- (Deck *)createDeck {
+    return [[PlayingCardDeck alloc] init];
+}
+
+- (void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([[sender currentTitle] length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"CardBack"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        Card *card = [self.deck drawRandomCard];
+        [sender setBackgroundImage:[UIImage imageNamed:@"CardFront"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:card.contents forState:UIControlStateNormal];
+    }
+
+    self.flipCount++;
+}
 
 @end
